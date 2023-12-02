600e86d59b5cff9c03d94a37b1d4d4511b1fd4e9
Baraka Bigega Espoir <49196039+espoirbaraka@users.noreply.github.com>
2023-12-02 13:30:29 +0100

    layout

--- src/app/page.js
+++ src/app/page.js
@@ -1,76 +1,16 @@
-"use client";
-import React, { useState } from 'react';
-import {
-  MenuFoldOutlined,
-  MenuUnfoldOutlined,
-  UploadOutlined,
-  UserOutlined,
-  VideoCameraOutlined,
-} from '@ant-design/icons';
-import { Layout, Menu, Button, theme } from 'antd';
-const { Header, Sider, Content } = Layout;
-const App = () => {
-  const [collapsed, setCollapsed] = useState(false);
-  const {
-    token: { colorBgContainer },
-  } = theme.useToken();
-  return (
-    <Layout>
-      <Sider trigger={null} collapsible collapsed={collapsed}>
-        <div className="demo-logo-vertical" />
-        <Menu
-          theme="dark"
-          mode="inline"
-          defaultSelectedKeys={['1']}
-          items={[
-            {
-              key: '1',
-              icon: <UserOutlined />,
-              label: 'nav 1',
-            },
-            {
-              key: '2',
-              icon: <VideoCameraOutlined />,
-              label: 'nav 2',
-            },
-            {
-              key: '3',
-              icon: <UploadOutlined />,
-              label: 'nav 3',
-            },
-          ]}
-        />
-      </Sider>
-      <Layout>
-        <Header
-          style={{
-            padding: 0,
-            background: colorBgContainer,
-          }}
-        >
-          <Button
-            type="text"
-            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
-            onClick={() => setCollapsed(!collapsed)}
-            style={{
-              fontSize: '16px',
-              width: 64,
-              height: 64,
-            }}
-          />
-        </Header>
-        <Content
-          style={{
-            margin: '24px 16px',
-            padding: 24,
-            minHeight: 280,
-            background: colorBgContainer,
-          }}
-        >
-          Content
-        </Content>
-      </Layout>
-    </Layout>
-  );
-};
+import React from 'react';
+import { Col, Row } from 'antd';
+const App = () => (
+  <Row>
+    <Col xs={2} sm={4} md={6} lg={8} xl={10}>
+      Col
+    </Col>
+    <Col xs={20} sm={16} md={12} lg={8} xl={4}>
+      Col
+    </Col>
+    <Col xs={2} sm={4} md={6} lg={8} xl={10}>
+      Col
+    </Col>
+  </Row>
+);
 export default App;
